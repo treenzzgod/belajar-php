d47be267b42dc6316984e574bac8d66e0f909fa8
treenzzgod <gio12619@gmail.com>
2022-11-07 11:04:55 +0700

    koneksi

---
+++ koneksi.php
@@ -0,0 +1,26 @@
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "fakultas";
+$koneksi = mysqli_connect($server, $username, $password, $database);
+$koneksi_db = mysqli_select_db($koneksi, $database);
+if ($koneksi){
+    echo "Koneksi berhasil<br>";
+    if ($koneksi_db){
+        echo "Database berhasil terhubung<br>";
+    } else {
+        echo "Database gagal terhubung <br>";
+    }
+}else{
+    echo "Koneksi gagal";
+}
+?>
+
+
+
+
+
+
+
+
